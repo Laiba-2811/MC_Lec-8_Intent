7a8f78b598939e7951c0862241293c52383ee268
mcsf19a002@pucit.edu.pk <mcsf19a002@pucit.edu.pk>
2021-04-03 22:34:28 +0500

    lec 8

---
+++ app/src/main/java/com/example/lecture8_intent/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.lecture8_intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Call(View view)
+    {
+        Uri uri= Uri.parse("tel:0301234567");
+        Intent intent=new Intent(Intent.ACTION_DIAL,uri);
+        startActivity(intent);
+
+    }
+    public void OpenWebsite(View view)
+    {
+        Uri uri= Uri.parse("http://www.pucit.edu.pk");
+        Intent intent=new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(intent);
+
+    }
+}
